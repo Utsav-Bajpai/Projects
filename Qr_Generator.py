b5d45a32561c6076c38d2bcf7ee3d6bb34009137
Utsav-Bajpai <utsavb736@gmail.com>
2024-12-08 16:57:31 +0530

    Update Qr_Generator.py

--- Qr_Generator.py
+++ Qr_Generator.py
@@ -3,12 +3,12 @@ from PIL import Image, ImageTk
 import qrcode
 
 def generate_qr():
-    data = entry.get("1.0", "end-1c")  # Get data from entry widget
+    data = entry.get("1.0", "end-1c")
     qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
     qr.add_data(data)
     qr.make(fit=True)
     qr_image = qr.make_image(fill_color="black", back_color="white")
-    qr_image.save("qrcode.png")  # Save the generated QR code
+    qr_image.save("qrcode.png")
     show_qr_code()
 
 def show_qr_code():
@@ -16,7 +16,7 @@ def show_qr_code():
     qr_image.thumbnail((300, 300))  
     qr_img = ImageTk.PhotoImage(qr_image)
     qr_label.config(image=qr_img)
-    qr_label.image = qr_img  # Keep a reference to prevent garbage collection
+    qr_label.image = qr_img
 
 root = tk.Tk()
 root.title("QR Code Generator")
